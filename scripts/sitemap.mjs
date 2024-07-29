24700e437beb67329e8e6cf8d23cbfdff96ea00d
Florian Martens <florian_martens@outlook.de>
2024-07-29 21:18:36 +0200

    Add sitemap and rss blog post

--- scripts/sitemap.mjs
+++ scripts/sitemap.mjs
@@ -9,11 +9,7 @@ export function getBaseUrl() {
 }
 
 export function formatDate(date) {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-
-  return `${year}-${month}-${day}`;
+  return date.toISOString().split(".")[0] + "+00:00";
 }
 
 try {
