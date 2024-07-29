88579dbfe477ae40ad14068471dd11364af92bb6
Florian Martens <florian_martens@outlook.de>
2024-07-30 01:20:34 +0200

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "../styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   );
 }
