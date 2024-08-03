6d199591d8d4e6f8b36cc3b18adc9920ac8439c4
Florian Martens <florian_martens@outlook.de>
2024-08-03 10:40:43 +0200

    Add RSS icon

--- scripts/rss.mjs
+++ scripts/rss.mjs
@@ -14,7 +14,7 @@ const feed = new RSS({
   title: "API-Fiddle Blog",
   feed_url: `${baseUrl}/rss.xml`,
   site_url: baseUrl,
-  image_url: "https://blog.api-fiddle.com/favicon.ico",
+  image_url: "https://blog.api-fiddle.com/rss-icon.png",
 });
 
 try {
