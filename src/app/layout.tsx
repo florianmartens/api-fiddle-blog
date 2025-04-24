17ea67dc7c54fe51da4a369e0a9d3e597f81706d
Florian Martens <florian_martens@outlook.de>
2025-04-24 08:51:33 +0200

    change cta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Analytics } from "@vercel/analytics/react";
+// import { Analytics } from "@vercel/analytics/react";
 import "../styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={inter.className}>{children}</body>
-      <Analytics />
+      {/* <Analytics /> */}
     </html>
   );
 }
--- src/components/cta.tsx
+++ src/components/cta.tsx
@@ -7,23 +7,23 @@ import Link from "next/link";
 
 export function Cta() {
   return (
-    <Link href="https://api-fiddle.com">
+    <Link href="https://pipe0.com">
       <div className="p-8 flex lg:gap-16 border rounded-lg items-center cursor-pointer flex-wrap">
         <div className="basis-full md:basis-1/3 shrink grow lg:block w-1">
-          <Logo width={200} height={100} />
+          <img width={200} height={100} src="https://pipe0.com/logo-dark.svg" />
         </div>
         <div className="basis-1/3 shrink grow py-4 w-1">
           <div className="">
             <h2 className="group cursor-pointer text-2xl pb-3 pt-6 font-semibold">
-              We were fed up with unclear API definitions and bad APIs
+              Add Clay-like data enrichment to your application
             </h2>
             <p className="pb-4">
-              So we created a better way. API-Fiddle is an API design tool with
-              first-class support for DTOs, versioning, serialization, suggested
-              response codes, and much more.
+              Pipe0 is the world&apos;s most powerful data enrichment framework.
+              Combine 50+ enrichment providers, web scraping and AI to find
+              company and people data.
             </p>
             <Button className="text-black bg-brand hover:bg-brand/80" size="lg">
-              Try, it&apos;s free <ArrowRight size={24} className="ml-3" />
+              Try, pipe0 <ArrowRight size={24} className="ml-3" />
             </Button>
           </div>
         </div>
